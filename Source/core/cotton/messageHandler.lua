8099daffecb75de8b7e35fd05915a6c9b8653c13
Bart Zaalberg <b.zaalberg@simgroep.nl>
2022-05-26 10:04:57 +0200

    Moved var within scope

--- Source/core/cotton/messageHandler.lua
+++ Source/core/cotton/messageHandler.lua
@@ -1,14 +1,16 @@
 import "./PromptIcon"
 
-local messaging = {
-    wordList = {},
-    chunkList = {},
-    currentLength = 0,
-    chunkOffset = 0,
-    currentChunk = 0
-}
-
-class("MessageHandler", {isActive = false}).extends()
+class(
+    "MessageHandler",
+    {
+        isActive = false,
+        wordList = {},
+        chunkList = {},
+        currentLength = 0,
+        chunkOffset = 0,
+        currentChunk = 0
+    }
+).extends()
 
 function MessageHandler:new(message, x, y, width, height)
     game.freeze()
@@ -34,13 +36,13 @@ function MessageHandler:new(message, x, y, width, height)
     self.textFontHeight = self.textFont:getHeight()
     self.numberOfLines = math.floor((self.dialogHeight - (self.margin * 2)) / self.textFontHeight)
 
-    messaging.wordList = self:splitMessageIntoWordsList(message)
-    messaging.chunkList = self:splitIntoChunksBasedOnWidth(self.dialogWidth - (self.margin * 2), self.textFont)
-    messaging.completeMessageLength = #message
-    messaging.chunkOffset = 1
-    messaging.currentChunk = 1
-    messaging.currentChunkLength = {}
-    messaging.isFullyRendered = false
+    self.wordList = self:splitMessageIntoWordsList(message)
+    self.chunkList = self:splitIntoChunksBasedOnWidth(self.dialogWidth - (self.margin * 2), self.textFont)
+    self.completeMessageLength = #message
+    self.chunkOffset = 1
+    self.currentChunk = 1
+    self.currentChunkLength = {}
+    self.isFullyRendered = false
 
     self.isActive = true
 end
@@ -66,41 +68,39 @@ function MessageHandler:drawMessage()
     local textPositionY = self.positionY + self.margin
 
     while (lineLoopIndex < self.numberOfLines) do
-        local chunk = messaging.chunkList[messaging.chunkOffset + lineLoopIndex]
+        local chunk = self.chunkList[self.chunkOffset + lineLoopIndex]
         if chunk == nil then
             break
         end
 
-        local truncatedMessage =
-            string.sub(chunk, 0, messaging.currentChunkLength[messaging.chunkOffset + lineLoopIndex] or 0)
+        local truncatedMessage = string.sub(chunk, 0, self.currentChunkLength[self.chunkOffset + lineLoopIndex] or 0)
         self.textFont:drawText(truncatedMessage, textPositionX, textPositionY + (self.textFontHeight * lineLoopIndex))
         lineLoopIndex = lineLoopIndex + 1
     end
 
-    if messaging.currentChunkLength[messaging.currentChunk] == #messaging.chunkList[messaging.currentChunk] then
-        if #messaging.chunkList == messaging.currentChunk then
-            if messaging.isFullyRendered then
+    if self.currentChunkLength[self.currentChunk] == #self.chunkList[self.currentChunk] then
+        if #self.chunkList == self.currentChunk then
+            if self.isFullyRendered then
                 return
             end
-            messaging.isFullyRendered = true
+            self.isFullyRendered = true
             -- End of all chunks!
             self.arrowDown:add()
             return
         end
 
-        if messaging.currentChunk % self.numberOfLines == 0 then
+        if self.currentChunk % self.numberOfLines == 0 then
             -- Next chunk page
             self.arrowDown:add()
             return
         end
 
         -- Next chunk
-        messaging.currentChunk = messaging.currentChunk + 1
+        self.currentChunk = self.currentChunk + 1
         return
     end
 
-    messaging.currentChunkLength[messaging.currentChunk] =
-        (messaging.currentChunkLength[messaging.currentChunk] or 0) + 1
+    self.currentChunkLength[self.currentChunk] = (self.currentChunkLength[self.currentChunk] or 0) + 1
 end
 
 function MessageHandler:detectInput()
@@ -111,13 +111,13 @@ function MessageHandler:detectInput()
             playdate.buttonJustPressed(playdate.kButtonLeft) or
             playdate.buttonJustPressed(playdate.kButtonRight)
      then
-        if #messaging.chunkList == messaging.currentChunk then
+        if #self.chunkList == self.currentChunk then
             -- End of all chunks!
             self:disableDialog()
             return
         end
 
-        if messaging.currentChunk % self.numberOfLines == 0 then
+        if self.currentChunk % self.numberOfLines == 0 then
             -- Next chunk page
             self:nextChunkPage()
 
@@ -131,19 +131,18 @@ end
 
 function MessageHandler:nextChunkPage()
     self.arrowDown:remove()
-    messaging.currentChunk = messaging.currentChunk + 1
-    messaging.chunkOffset = messaging.chunkOffset + self.numberOfLines
+    self.currentChunk = self.currentChunk + 1
+    self.chunkOffset = self.chunkOffset + self.numberOfLines
 end
 
 function MessageHandler:skipTransition()
     local lineLoopIndex = 0
 
     while (lineLoopIndex < self.numberOfLines) do
-        if messaging.chunkList[messaging.chunkOffset + lineLoopIndex] == nil then
+        if self.chunkList[self.chunkOffset + lineLoopIndex] == nil then
             break
         end
-        messaging.currentChunkLength[messaging.chunkOffset + lineLoopIndex] =
-            #messaging.chunkList[messaging.chunkOffset + lineLoopIndex]
+        self.currentChunkLength[self.chunkOffset + lineLoopIndex] = #self.chunkList[self.chunkOffset + lineLoopIndex]
         lineLoopIndex = lineLoopIndex + 1
     end
 end
@@ -160,7 +159,7 @@ function MessageHandler:splitIntoChunksBasedOnWidth(maxTextWidth, textFont)
     local chunks = {}
     local currentChunk = 1
 
-    for key, word in pairs(messaging.wordList) do
+    for key, word in pairs(self.wordList) do
         if chunks[currentChunk] == nil then
             chunks[currentChunk] = word
         elseif textFont:getTextWidth(chunks[currentChunk] .. " " .. word) <= maxTextWidth then
